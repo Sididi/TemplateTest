d09330069197a03d78e80cc9ae5c49cace4c040f
romain <romain.pillot@epitech.eu>
2020-11-27 19:15:18 +0100

    commit

--- src/main/kotlin/com/redpills/correction/framework/Worker.kt
+++ src/main/kotlin/com/redpills/correction/framework/Worker.kt
@@ -28,9 +28,8 @@ open class Worker {
         while (reader.readLine().also { line = it } != null)
             containerId += line
 
-        println("CONTAINER ID IS $containerId")
         this.containerId = ContainerIdentityDTO(containerId)
-        this.studentRepository = Http.post("$serviceUrl/init", containerId).toResponse(GitRepository::class.java)
+        this.studentRepository = Http.post("$serviceUrl/init", this.containerId).toResponse(GitRepository::class.java)
             ?: error("internal error while trying to retrieve student repository")
     }
 
