cfac9f10f5d9812ac59b4fdddbba6ae437ec84f4
romain <romain.pillot@epitech.eu>
2020-11-27 15:53:49 +0100

    commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.20"
     id("com.github.johnrengelman.shadow") version("6.1.0")
 }
 
