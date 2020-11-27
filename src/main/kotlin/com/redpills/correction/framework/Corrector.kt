b69950b970b13d0ed88cddb257bafc3879fec5e7
romain <romain.pillot@epitech.eu>
2020-11-27 19:36:40 +0100

    commit

--- src/main/kotlin/com/redpills/correction/framework/Corrector.kt
+++ src/main/kotlin/com/redpills/correction/framework/Corrector.kt
@@ -1,6 +1,7 @@
 package com.redpills.correction.framework
 
 import com.redpills.correction.framework.domain.Task
+import com.redpills.correction.framework.dto.WorkerInnerTestResultDTO
 import com.redpills.correction.framework.dto.WorkerTestResultDTO
 import com.redpills.correction.framework.dto.WorkerTestResultStateDTO
 import com.redpills.correction.framework.helpers.Git
@@ -10,7 +11,7 @@ class Corrector(
     val tasks: List<Task>,
     val compileScript: String
 ): Worker() {
-    val results = WorkerTestResultDTO(containerId, "Tests", mutableListOf())
+    val results = WorkerTestResultDTO(containerId, "Tests", "", mutableListOf())
     lateinit var studentDirectory: File
 
     fun launch() {
@@ -34,14 +35,25 @@ class Corrector(
     }
 
     private fun executeTests() {
+        var markObtained = 0
+        var markMax = 0
+
         val marks = tasks.flatMap { it.execute(studentDirectory) }.map {
-            val status = when (it.mark) {
-                0 -> WorkerTestResultStateDTO.FAILED
-                else -> WorkerTestResultStateDTO.PASSED
+            val status: WorkerTestResultStateDTO;
+            markMax += it.mark;
+
+            when (it.mark) {
+                0 -> status = WorkerTestResultStateDTO.FAILED
+                else -> {
+                    status = WorkerTestResultStateDTO.PASSED
+                    markObtained += it.mark
+                }
             }
 
-            WorkerTestResultDTO(containerId, it.hint, mutableListOf(), status)
+            WorkerInnerTestResultDTO(it.hint, mutableListOf(), status, it.mark)
         }
+
+        results.mark = "$markObtained/$markMax"
 
         results.children.addAll(marks)
     }
